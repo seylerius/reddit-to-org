bd02c510fbb5e5418217a4a185094817127f5977
Seylerius <seylerius@publicinquisition.com>
2016-08-03 21:26:40 -0700

    Identified reddit.clj as the viable API wrapper

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [reddit.clj "0.4.0"]]
   :main ^:skip-aot com.seriouslyseylerius.reddit-to-org
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
