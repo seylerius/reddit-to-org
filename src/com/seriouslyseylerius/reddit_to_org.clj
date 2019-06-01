a5bc4daab9c001b1fc8c963a87303a91f68da7c1
Seylerius <seylerius@publicinquisition.com>
2019-06-01 06:36:24 -0500

    Update project notes

--- src/com/seriouslyseylerius/reddit_to_org.clj
+++ src/com/seriouslyseylerius/reddit_to_org.clj
@@ -1,11 +1,32 @@
 (ns com.seriouslyseylerius.reddit-to-org
-  (:require [clojure.data.json :as json])
+  (:require [clojure.data.json :as json]
+            [reddit.clj.core :as reddit])
   (:gen-class))
+
+;; (defn append-saved
+;;   "Get the next grouping of saved items, optionally stopping")
+
+;; (defn get-saved
+;;   "Retrieve saved items, optionally stopping on a particular item."
+;;   ([rc]
+;;    )
+;;   (loop [count 0 after nil data (reddit/saved rc)] (let [c (+ count 25) a (:name (last data))] (println data) (if data (recur c a (reddit/saved rc c a))))))
+
+(defn saved
+  "Get saved items from reddit, marking the end count and the ID of the tail."
+  ([rc]
+   (let [page (reddit/saved rc)]
+     {:page page
+      :next [25 (:name (last page))]}))
+  ([rc [count after]]
+   (let [page (reddit/saved rc count after)]
+    {:page page
+     :next [(+ count 25) (:name (last page))]})))
 
 (defn filter-json
   "Filter reddit json down to just title, text & links"
   [data]
-  (let [useful-keys [:domain :subreddit :selftext :url :title :link_title :body :link_url :link_id :id]]
+  (let [useful-keys [:domain :subreddit :selftext :url :title :link_title :body :link_url :link_id :id :name]]
     (->> data
         :data
         :children
